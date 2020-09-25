9611bfc47e96b78b7e35bbed4a5061a1f58b958d
Rongzhi Wang <aidenwang0826@gmail.com>
2020-09-25 16:36:35 +1000

    add sign in function

--- src/signin.js
+++ src/signin.js
@@ -1,18 +1,118 @@
-import React from "react";
+/*
+ * @Author: Aiden
+ * @Date: 2020-09-25 11:41:03
+ * @LastEditTime: 2020-09-25 16:27:12
+ * @LastEditors: Please set LastEditors
+ * @Description: Sign in Page
+ * @FilePath: /shopping-cart-app/src/signin.js
+ */
+
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(5),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+
+  submit: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 function Signin() {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    password: "",
+    showPassword: false,
+  });
+  const handleChange = (prop) => (e) => {
+    setValues({ ...values, [prop]: e.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="signin">
-      <Container maxWidth="sm" />
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            id="outlined-username-input"
+            label="Username"
+            type="username"
+            autoComplete="current-username"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+          />
+          <FormControl variant="outlined" required fullWidth margin="normal">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+          </FormControl>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            size="large"
+            className={classes.submit}
+          >
+            Sign in
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 }
 
