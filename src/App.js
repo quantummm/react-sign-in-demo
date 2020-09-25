05d1258765f0099fa6331833973d5e13ce224c05
Rongzhi Wang <aidenwang0826@gmail.com>
2020-09-25 16:36:24 +1000

    add appbar on homepage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Aiden
+ * @Date: 2020-09-25 11:10:56
+ * @LastEditTime: 2020-09-25 16:05:02
+ * @LastEditors: Please set LastEditors
+ * @Description: Main page
+ * @FilePath: /shopping-cart-app/src/App.js
+ */
+
 import React from "react";
 import Header from "./header";
 import Signin from "./signin";
--- src/header.js
+++ src/header.js
@@ -1,9 +1,55 @@
+/*
+ * @Author: Aiden
+ * @Date: 2020-09-25 11:31:20
+ * @LastEditTime: 2020-09-25 16:03:13
+ * @LastEditors: Please set LastEditors
+ * @Description: create an appbar
+ * @FilePath: /shopping-cart-app/src/header.js
+ */
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import { Toolbar, IconButton, Typography } from "@material-ui/core";
+import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Header() {
+  const classes = useStyles();
   return (
     <div className="header">
-      <header className="App-header"></header>
+      <header></header>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuRoundedIcon fontSize="large" />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Shopping Cart App
+          </Typography>
+          <IconButton
+            edge="end"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="account"
+          >
+            <AccountCircleRoundedIcon fontSize="large" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
