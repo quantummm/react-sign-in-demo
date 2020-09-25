56f4e38912478a06c377dd237bb7c48ec434fa48
Rongzhi Wang <aidenwang0826@gmail.com>
2020-09-25 11:54:08 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Signin from "./signin";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/signin.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+
+function Signin() {
+  return (
+    <div className="signin">
+      <Container maxWidth="sm" />
+    </div>
+  );
+}
+
+export default Signin;
